ffc183dbd816febb24702745c7a510f7cf6880e2
ShyroVO <bosquet.laurie@outlook.com>
2020-12-18 15:57:44 +0100

    0.1

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ let tableauUn = [1, 2, 3, 4, 5];
 let tableauDeux = ["valeur1", "valeur2", "valeur3", "valeur4", "valeur5", "bidulemachin", "tatayoyo"];
 
 /* Votre code ici bas */
+
+let tableauExposant = tableau => tableau % tableau;
+
