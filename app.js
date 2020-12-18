937026680ba45bdd2cfd409bbc6b91a84f8cd245
ShyroVO <bosquet.laurie@outlook.com>
2020-12-18 16:16:10 +0100

    0.3

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ let tableauUn = [1, 2, 3, 4, 5];
 let tableauDeux = ["valeur1", "valeur2", "valeur3", "valeur4", "valeur5", "bidulemachin", "tatayoyo"];
 
 /* Votre code ici bas */
+let tableauExposant = tableauUn.map(x => x % x);
+let tableauMultiplication = tableauUn.map(x => x * 10);
+let tableauAjoutDivi = tableauUn.map(x => (x + 2) /22);
 
-let tableauExposant = tableau => tableau % tableau;
 
-let tableauT = tableauExposant(tableauUn);
 
-console.log(tableauT)
+
+console.log(tableauExposant);
+console.log(tableauMultiplication);
+console.log(tableauAjoutDivi);
+
+
