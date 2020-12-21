6b6f970092aac171fc12c024921f9ec10e84f28a
ShyroVO <Bosquet.laurie@outlook.com>
2020-12-21 21:24:45 +0100

    1.0

--- app.js
+++ app.js
@@ -2,26 +2,47 @@ let tableauUn = [1, 2, 3, 4, 5];
 let tableauDeux = ["valeur1", "valeur2", "valeur3", "valeur4", "valeur5", "bidulemachin", "tatayoyo"];
 
 /* Votre code ici bas */
+let bebeDiv = document.createElement('div');
+document.body.appendChild(bebeDiv);
+
 // Tableau 1:
-let tableauExposant = tableauUn.map(x => x % x);
+let tableauExposant = tableauUn.map(x => x ** x);
+bebeDiv.innerHTML += tableauExposant + "<br>";
+
 let tableauMultiplication = tableauUn.map(x => x * 10);
-let tableauAjoutDivi = tableauUn.map(x => (x + 2) /22);
+bebeDiv.innerHTML += tableauMultiplication + "<br>";
 
-let tableauFiltre = tableauUn.map(x => x > 2);
+let tableauAjoutDivi = tableauUn.map(x => (x + 2) /21);
+bebeDiv.innerHTML += tableauAjoutDivi + "<br>";
 
-console.log(tableauExposant);
-console.log(tableauMultiplication);
-console.log(tableauAjoutDivi);
+let tableauFiltreSup2 = tableauUn.filter(x => x > 2);
+bebeDiv.innerHTML += tableauFiltreSup2 + "<br>";
 
-console.log(tableauFiltre);
+let tableauFiltreDiv = tableauUn.filter(x => 0 === (x % 2));
+bebeDiv.innerHTML += tableauFiltreDiv + "<br>";
+
+let tableauFiltreMul3 = tableauUn.filter(x => (x * 3) > 10);
+bebeDiv.innerHTML += tableauFiltreMul3 + "<br>";
 
 // Tableau 2:
+let bebeDiv2 = document.createElement('div');
+document.body.appendChild(bebeDiv2);
+
 let tableauLongueur = tableauDeux.map(x => x.length);
+bebeDiv.innerHTML += tableauLongueur + "<br>";
+
 let tableauAddLongVal = tableauDeux.map (x => x + (x.length));
+bebeDiv.innerHTML += tableauAddLongVal + "<br>";
+
 let tableauConca = tableauDeux.map (x => x + x);
+bebeDiv.innerHTML += tableauConca+ "<br>";
 
-console.log(tableauLongueur);
-console.log(tableauAddLongVal);
-console.log(tableauConca);
+let tableauLongSup2 = tableauDeux.filter( x => x.length > (2 + 2));
+bebeDiv.innerHTML += tableauLongSup2 + "<br>";
 
+let tableauDiv2 = tableauDeux.filter( x => 0 === (x.length % 2) );
+bebeDiv.innerHTML += tableauDiv2 + "<br>";
+
+let tableauLongMin = tableauDeux.filter( x => (x.length - 3) > 10 );
+bebeDiv.innerHTML += tableauLongMin + "<br>";
 
