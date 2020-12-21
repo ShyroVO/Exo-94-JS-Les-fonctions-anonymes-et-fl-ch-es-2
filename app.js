de54ed7678b261be671503ba273317c1b944b8f8
ShyroVO <bosquet.laurie@outlook.com>
2020-12-21 12:30:04 +0100

    .

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ let tableauUn = [1, 2, 3, 4, 5];
 let tableauDeux = ["valeur1", "valeur2", "valeur3", "valeur4", "valeur5", "bidulemachin", "tatayoyo"];
 
 /* Votre code ici bas */
-
 // Tableau 1:
 let tableauExposant = tableauUn.map(x => x % x);
 let tableauMultiplication = tableauUn.map(x => x * 10);
